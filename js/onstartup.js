5884face3b09ed8f7aba432e455f7a9738464555
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-04-23 20:34:09 -0300

    Actualizacion descargas

--- js/onstartup.js
+++ js/onstartup.js
@@ -69,13 +69,13 @@ $.getJSON("https://api.github.com/repos/ppizarror/Template-Informe/releases", fu
     console.log(String.format('Última versión - enlace descarga: {0}', last_version_link));
 
     // Se añaden las 129 descargas de https://api.github.com/repos/ppizarror/Template-Informe-cursos/releases
-    // Se añaden 60 descargas entre 1.8.5 y 2.0.7
+    // Se añaden 60 descargas entre 1.8.5 y 1.9.6
+    // Se añaden 138 descargas entre 1.9.6 y 2.0.7
     if (total_downloads == 0){
         total_downloads = 'NaN';
     }
     else{
-        total_downloads += 129;
-        total_downloads += 60;
+        total_downloads += 129 + 60 + 138;
     }
 
     // Se establece la versión en el contador de descargas totales
