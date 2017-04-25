6958b7cb42497f6e23a521b1be94b787b8d818e4
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-04-24 22:17:15 -0300

    Actualizacion a version 3.2.0

--- js/onstartup.js
+++ js/onstartup.js
@@ -72,11 +72,12 @@ $.getJSON("https://api.github.com/repos/ppizarror/Template-Informe/releases", fu
     // Se añaden 60 descargas entre 1.8.5 y 1.9.6
     // Se añaden 138 descargas entre 1.9.6 y 2.0.6
     // Se añaden 3 descargas de version 2.0.7
+    // Se añaden 3 descargas de version 2.0.8
     if (total_downloads == 0){
         total_downloads = 'NaN';
     }
     else{
-        total_downloads += 129 + 60 + 138 + 3;
+        total_downloads += 129 + 60 + 138 + 3 + 3;
     }
 
     // Se establece la versión en el contador de descargas totales
