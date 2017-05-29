7fff753162874d37fb4eef1549f13d454363a23f
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-05-29 18:57:27 +0100

    Se agrega la version 3.8.7

--- js/utils.js
+++ js/utils.js
@@ -65,7 +65,8 @@ function updateDownloadCounter(downloads) {
     // Se agregan 60 de version 2.3.7-2.4.0
     // Se agregan 3 descargas de version 2.4.1
     // Se agregan 20 descargas de version 2.4.2-2.4.5
-    download_list_counter = [129, 60, 138, 3, 3, 1, 4, 55, 115, 74, 17, 3, 18, 7, 68, 71, 12, 9, 4, 33, 1, 60, 3, 20];
+    // Se agregan 38 descargas de version 2.4.6
+    download_list_counter = [129, 60, 138, 3, 3, 1, 4, 55, 115, 74, 17, 3, 18, 7, 68, 71, 12, 9, 4, 33, 1, 60, 3, 20, 38];
 
     // Se añaden las descargas de template-informe-cursos
     // $.getJSON("https://api.github.com/repos/ppizarror/Template-Informe-cursos/releases", function(json) {
