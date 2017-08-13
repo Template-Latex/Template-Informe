afeff8a4f8e311264418eb9f599339bee37dc6d7
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-08-13 03:42:28 +0100

    Se actualiza contador, correccion apartado referencias

--- js/utils.js
+++ js/utils.js
@@ -78,7 +78,8 @@ function updateDownloadCounter(downloads) {
 	// Se agregan 31 descargas de version 3.2.1
 	// Se agregan 2 descargas de version 3.2.2
     // Se agregan 8 descargas de versión 3.2.3
-    download_list_counter = [129, 60, 138, 3, 3, 1, 4, 55, 115, 74, 17, 3, 18, 7, 68, 71, 12, 9, 4, 33, 1, 60, 3, 20, 38, 6, 146, 62, 28, 28, 84, 3, 12, 110, 31, 2, 8];
+    // Se agregan 110 descargas de versión 3.2.4-3.3.0
+    download_list_counter = [129, 60, 138, 3, 3, 1, 4, 55, 115, 74, 17, 3, 18, 7, 68, 71, 12, 9, 4, 33, 1, 60, 3, 20, 38, 6, 146, 62, 28, 28, 84, 3, 12, 110, 31, 2, 8, 110];
 
     for (i = 0; i < download_list_counter.length; i++) {
         downloads += download_list_counter[i];
