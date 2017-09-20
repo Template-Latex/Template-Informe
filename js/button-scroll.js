2b36e85f710b9b47842444f84f3f70b0885311da
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-09-20 03:01:09 +0100

    Se borran archivos sin usar

--- js/button-scroll.js
+++
@@ -1,9 +0,0 @@
-var amountScrolled = 600;
-$(window).scroll(function() {
-    location.pathname.replace(/^\//, '')
-    if ($(window).scrollTop() > amountScrolled) {
-        $('a.back-to-top').fadeIn('slow');
-    } else {
-        $('a.back-to-top').fadeOut('slow');
-    }
-});
