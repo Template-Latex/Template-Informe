49e643b0f325131c378fef2e6546d6ccbc830d91
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-09-23 21:47:36 +0100

    Se borra carpeta resources

--- js/config.js
+++ js/config.js
@@ -27,6 +27,6 @@ var href_github_project_source = 'https://github.com/Template-Latex/Template-Inf
 var href_images_parent = 'images/';
 var href_json_releases = 'https://api.github.com/repos/Template-Latex/Template-Informe/releases';
 var href_pdf_version = '../../pdf-version/Informe/Template-Informe v{0}.pdf';
-var href_resources_folder = 'resources/';
+var href_resources_folder = 'http://latex.ppizarror.com/resources/';
 var stats_name = 'Informe';
 var update_download_counter = 'Template-Informe';
