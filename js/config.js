3e869cfac09e6cd1103580f5d9e055ed75d162b5
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-09-24 03:59:55 +0100

    Se elimina variable sin usar

--- js/config.js
+++ js/config.js
@@ -24,7 +24,6 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // Configuraciones
 var href_github_project = 'https://github.com/Template-Latex/Template-Informe/releases/';
 var href_github_project_source = 'https://github.com/Template-Latex/Template-Informe/';
-var href_images_parent = 'images/';
 var href_json_releases = 'https://api.github.com/repos/Template-Latex/Template-Informe/releases';
 var href_pdf_version = '../../pdf-version/Informe/Template-Informe v{0}.pdf';
 var href_resources_folder = 'http://latex.ppizarror.com/resources/';
