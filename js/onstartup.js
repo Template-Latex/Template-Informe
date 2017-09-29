4e861814e75a734daf1ba54e8810b4a4f4ddea88
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-09-29 15:55:39 +0100

    Se transfiere smooth scroll a onstartup

--- js/onstartup.js
+++ js/onstartup.js
@@ -255,6 +255,22 @@ jQuery(document).ready(function($) {
         }
     });
 
+    // Smooth scrolling al clickear un anchor
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 700);
+                    return false;
+                }
+            }
+        });
+    });
+
     // Se llama a la función de cada template después de cargar
     afterDocumentReady();
 });
