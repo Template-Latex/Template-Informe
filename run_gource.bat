1e6d0e1c211fd3cc6390aff635cc6ed97c6d23ca
ppizarror <pablo.pizarro@ing.uchile.cl>
2020-06-05 23:18:01 -0400

    Añade gráfico gource

---
+++ run_gource.bat
@@ -0,0 +1,5 @@
+@echo off
+
+gource -s 0.05 --title Template-Informe --disable-auto-rotate --output-framerate 60 --key --highlight-users --disable-bloom --multi-sampling -w --transparent --path ./
+
+@echo on
