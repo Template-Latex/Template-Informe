7f939289c009ed64be726d8686fc549a1be24ac4
ppizarror <pablo@ppizarror.com>
2021-10-30 19:38:52 -0300

    Actualiza lista de unicodes

--- test/unicode.py
+++ test/unicode.py
@@ -2,11 +2,7 @@
 Generate unicode tests.
 """
 
-char = chr(int('0x0393', 16))
-print("here is your checkmark: " + char)
-
 myunicodes=''.join(open('src/cfg/unicode.tex', 'r').readlines())
-print(myunicodes)
 
 f = open('test/unicode.sty', 'r').readlines()
 newkcodes = []
@@ -16,20 +12,23 @@ for j in f:
 		continue
 	if '%' in j.strip()[0]:
 		continue
-	if 'DeclareUnicodeCharacter' not in j or '\\def' in j:
+	if 'DeclareUnicodeCharacter' not in j or '\\def' in j or '\\newcommand' in j:
 		continue
 	kcode = j.split('}{')[0].split('{')[1]
 	if f'{kcode}' in myunicodes:
-		print(f'{kcode} repeated')
+		# print(f'{kcode} repeated')
 		continue
+	if '%' in j:
+		j = j.split('%')[0].strip()
 	newkcodes.append(j)
 
+newkcodes.sort()
 print('New kcodes')
 for j in newkcodes:
 	print(j)
 
 # Iterate through unicodes
-write_test = False
+write_test = True
 
 if write_test:
 	f = open('test/unicode.tex', 'w')
@@ -37,6 +36,8 @@ if write_test:
 	added = []
 	for j in myunicodes.split('\n'):
 		if 'DeclareUnicodeCharacter' not in j or '\\def' in j or '\\ifdefined' in j:
+			continue
+		if j[0] == '%':
 			continue
 		kcode = j.split('}{')[0].split('{')[1]
 		if kcode not in added:
@@ -49,29 +50,42 @@ if write_test:
 	f.close()
 
 f = open('test/unicode_replacer.py', 'w')
+cmd = []
 notcmd = []
+addedjval = []
 for j in myunicodes.split('\n'):
 	if 'DeclareUnicodeCharacter' not in j or '\\def' in j or '\\ifdefined' in j:
 		continue
-	kcode = j.split('}{')[0].split('{')[1]
-	jval = j.split('}{')[1].strip()[0:-1]
+	jsp = j.split('}{')
+	kcode = jsp.pop(0).split('{')[1]
+	jval = '}{'.join(jsp).strip()[0:-1]
 	char = chr(int(f'0x{kcode}', 16))
 	if '\\ensuremath' in jval:
 		jval = jval.replace('\\ensuremath{', '')[0:-1]
 	if jval[0] == '{':
 		continue
-	if 'NOT' in jval or 'NONE' in jval:
+	if 'NOT' in jval or 'NONE' in jval or '\LOCALunknownchar' in jval:
 		continue
-	if '\hbox' in jval or '\else' in jval or '{ }' in jval:
+	if '\hbox' in jval or '\else' in jval or '{ }' in jval or '\\,' in jval or '\\text{' in jval or '!' in jval:
 		continue
 	jval = jval.replace('\\', '\\\\')
+	if jval in addedjval:
+		# print(f'REPEATED {jval}')
+		continue
+	addedjval.append(jval)
 	txt = f"\t('{jval}', '{char}'),\n"
 	if jval == char:
 		continue
 	if '\\' not in jval:
+		if len(jval) == 1 or "'" in jval:
+			continue
 		notcmd.append(txt)
-		continue
-	f.write(txt)
+	else:
+		cmd.append(txt)
+cmd.sort(key=lambda v: v.upper())
+notcmd.sort(key=lambda v: v.upper())
+for j in cmd:
+	f.write(j)
 for j in notcmd:
 	f.write(j)
 f.close()
