f0b77911d755eb089fb5ab12bcd14c709c0dbdff
ppizarror <pablo@ppizarror.com>
2024-08-11 17:56:55 +0100

    Elimina gource

--- run_gource.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-gource -s 0.05 --title Template-Informe --disable-auto-rotate --output-framerate 60 --key --highlight-users --disable-bloom --multi-sampling -w --transparent --path ./
-
-@echo on
